ce24cd91184ac107853edce3b76225af9e9c6e21
sheikhfaris86 <sheikhfaris86@gmail.com>
2025-07-11 21:20:34 +0530

    added a loading page for dynamic bg routes

---
+++ src/app/backgrounds/loading.tsx
@@ -0,0 +1,9 @@
+// add framer for smooth fade out
+
+export default function loading() {
+  return (
+    <div className="w-full flex items-center justify-center">
+      <p>loading...</p>
+    </div>
+  );
+}
